9e1d2b8e5ee12e739612da02f27bbfaec1c3fe83
marc <cece.marc.vu@gmail.com>
2017-07-04 09:22:01 +0200

    feat: game init functions

--- game/models.py
+++ game/models.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 from django.db import models
 
 
@@ -23,6 +25,44 @@ class Continent(models.Model):
 class Game(models.Model):
     is_active = models.BooleanField(default=True)
 
+    def init(self, players):
+        self._set_players(players)
+        self._distribute_territories()
+        self._distribute_reinforcements()
+
+    def start(self, players):
+        pass
+
+    def _set_players(self, players):
+        for player in players:
+            p, _ = Player.objects.get_or_create(slack_id=player)
+            PlayerGame.objects.create(game=self, player=p, color='black')
+
+    def _distribute_territories(self):
+        players = self.playergame_set.all()
+        n_players = len(players)
+        territories = list(Territory.objects.all())
+        shuffle(territories)
+        for i, territory in enumerate(territories):
+            TerritoryGame.object.create(
+                game=self, territory=territory, owner=players[i % n_players])
+
+    def _distribute_reinforcements(self):
+        players = self.playergame_set.all()
+        n_players = len(players)
+        reinforcements = 35
+
+        if n_players == 4:
+            reinforcements = 30
+        elif n_players == 5:
+            reinforcements = 25
+        elif n_players == 6:
+            reinforcements = 20
+
+        for player in players:
+            player.reinforcements = reinforcements
+            player.save()
+
 
 class PlayerGame(models.Model):
     game = models.ForeignKey("Game")
@@ -30,7 +70,7 @@ class PlayerGame(models.Model):
     color = models.CharField(max_length=50)
     is_active = models.BooleanField(default=False)
     turn_order = models.IntegerField(default=0)
-    reinforcement = models.IntegerField(default=0)
+    reinforcements = models.IntegerField(default=0)
 
     def owns_continents(self):
         for continent in Continent.objects.all():
@@ -48,4 +88,5 @@ class TerritoryGame(models.Model):
     game = models.ForeignKey("Game")
     territory = models.ForeignKey("Territory")
     owner = models.ForeignKey("PlayerGame")
-    army = models.IntegerField(default=0)
+    army = models.IntegerField(default=1)
+    new_army = models.IntegerField(default=0)
