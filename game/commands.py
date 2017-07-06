1be53b0dc50c396b04fdcb874c80cf03dea2d0fa
marc <cece.marc.vu@gmail.com>
2017-07-06 09:48:48 +0200

    feat:

--- game/commands.py
+++ game/commands.py
@@ -6,6 +6,7 @@ class ValidationException(Exception):
 
 
 class Command:
+    AVAILABLE_COMMANDS = ['new', 'start', 'reinforce', 'attack', 'move']
 
     def __init__(self, data):
         self.user_id = data['user_id']
@@ -26,7 +27,15 @@ class Command:
         if len(text) == 0:
             raise ValidationException('Empty command')
         text = text.split(' ', 1)
-        action = text[0]
+        action = text[0].lower()
         arguments = text[1].strip() if len(text) > 1 else ''
 
         return action, arguments
+
+    def validate_command(action, arguments):
+        if action not in Command.AVAILABLE_COMMANDS:
+            raise ValidationException('Unknown Command')
+
+
+class GameCommand:
+    pass
